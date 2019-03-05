2e184cf203a51f7677ebb2054e836942ccacbc00
Сергей <sewer007@mail.ru>
2019-03-05 22:40:48 +0300

    Добавлено решение

---
+++ GlasfullPlugin.Libary/KompasConnector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Kompas6API5;
+
+namespace GlassfullPlugin.Libary
+{
+    /// <summary>
+    /// Класс для подключения к Компас 3D.
+    /// </summary>
+    public class KompasConnector
+    {
+        /// <summary>
+        /// Интерфейс API КОМПАС 3D.
+        /// </summary>
+        public KompasObject Kompas { get; set; }
+        
+        /// <summary>
+        /// Запуск Компас 3D.
+        /// </summary>
+        public void OpenKompas()
+        {
+            if (Kompas == null)
+            {
+                var type = Type.GetTypeFromProgID("KOMPAS.Application.5");//поиск в пространстве COM-объектов объекта со следующим названием.
+                Kompas = (KompasObject)Activator.CreateInstance(type);//Создает экземпляр этого приложения и получает на него ссылку. 
+            }
+
+            if (Kompas != null)
+            {
+                Kompas.Visible = true;//Делаем его видимым. 
+                Kompas.ActivateControllerAPI();//Передаем управление API. 
+            }
+        }
+
+        /// <summary>
+        /// Закрыть Компас 3D.
+        /// </summary>
+        public void CloseKompas()
+        {
+            try
+            {
+                Kompas.Quit();
+                Kompas = null;
+            }
+            catch
+            {
+                Kompas = null;
+            }
+        }
+    }
+}
+
